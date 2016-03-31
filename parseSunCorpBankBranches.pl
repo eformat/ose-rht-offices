26523a59f3b1639b58ffe201c0e69ad2e960de73
eformat <eformat@gmail.com>
2016-03-31 21:29:19 +1300

    suncorp data

--- parseSunCorpBankBranches.pl
+++ parseSunCorpBankBranches.pl
@@ -1,12 +1,20 @@
 #!/bin/perl
 use strict;
-use Text::ParseWords;
+use Data::Dumper;
+use JSON;
     
-my $filename = "Centrelink-Office-Locations-as-at--3-March-2016.csv";
-open (my $fh, "<$filename") || die "can't open $filename $!";
-while (<$fh>) {
-    #print $_;
-    my ($office_type,$site_name,$alternative_name,$address,$suburb,$state,$postcode,$latitude,$longitude,$open,$close,$closed_for_lunch) 
-	= parse_line(q{,}, 0, $_);
-    print '{ "name": "'. $site_name .'", "city": "'. $suburb .'", "code": "'. $site_name .'", "street": "'. $address .'", "state": "'. $state .'", "zip": "'. $state .'", "phone": "+61 555 555 5555", "fax": "+61 555 555 5555", "coordinates":[ '. $longitude .','. $latitude .' ]},' . "\n";
+my $filename = "suncorp-branches";
+open (SLURP, "<${filename}") or die ("unable to open $filename ($!)");
+my $json;
+{
+    local $/ = undef;
+    $json = <SLURP>;
+    close (SLURP);
+}
+
+my $perl = from_json($json);
+#print Data::Dumper->Dump([$perl->{'locations'}]);
+
+foreach $b ( @{$perl->{'locations'}} ) {
+    print '{ "name": "'. $b->{'BranchName'} .'", "city": "'. $b->{'Suburb'} .'", "code": "'. $b->{'BranchId'} .'", "street": "'. $b->{'Street1'} .'", "state": "'. $b->{'State'} .'", "zip": "'. $b->{'PostCode'} .'", "phone": "'. $b->{'BranchPhone'} .'", "fax": "'. $b->{'BranchPhone'} .'", "coordinates":[ '. $b->{'Longitude'} .','. $b->{'Latitude'} .' ]},' . "\n";
 }
